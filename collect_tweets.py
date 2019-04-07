9755fce622fc8e95a53e8c99bbb2f4f400e25564
Tim Renner <renner.timothy@gmail.com>
2019-04-07 14:07:47 -0500

    Pull collection logic into its own file.

---
+++ collect_tweets.py
@@ -0,0 +1,129 @@
+import twitter
+
+from toolz import get_in, curry, thread_last, partition_all
+from elasticsearch.helpers import bulk as es_bulk
+from loguru import logger
+
+TWEET_MAPPING = {
+    "mappings": {
+        "tweet": {
+            "properties": {
+                "coordinates": {"type": "geo_point"},
+                "created_at": {
+                    "type": "date",
+                    "format": "EEE MMM dd HH:mm:ss Z yyyy",
+                },
+            }
+        }
+    }
+}
+
+
+PROFANITY = [
+    "fuck",
+    "shit",
+    "bitch",
+    "dick",
+    "douche",
+    " ass ",
+    "asshole",
+    "asshat",
+    "jackass",
+    "dumbass",
+]
+
+
+def _extract_text(tweet):
+    if "retweeted_status" in tweet:
+        if get_in(["retweeted_status", "truncated"], tweet, False):
+            return tweet["retweeted_status"]["extended_tweet"]["full_text"]
+        else:
+            return tweet["retweeted_status"]["text"]
+    else:
+        return tweet["text"]
+
+
+def _contains_profanity(tweet):
+    tweet_text = _extract_text(tweet).lower()
+    for profanity in PROFANITY:
+        if profanity in tweet_text:
+            return True
+
+    return False
+
+
+def _tweet_to_bulk(index, tweet):
+    return {
+        "_index": index,
+        "_type": "tweet",
+        "_id": tweet["id_str"],
+        "_source": {
+            "id": tweet["id_str"],
+            "coordinates": get_in(["coordinates", "coordinates"], tweet, None),
+            "text": _extract_text(tweet),
+            "created_at": tweet["created_at"],
+        },
+    }
+
+
+def collect_tweets(
+    es_client,
+    track,
+    twitter_consumer_key,
+    twitter_consumer_secret,
+    twitter_access_token_key,
+    twitter_access_token_secret,
+    elasticsearch_index="profanity-power-index",
+    drop_index=False,
+    batch_size=10,
+):
+
+    if es_client.indices.exists(elasticsearch_index):
+        logger.warning(f"Index {elasticsearch_index} exists.")
+        if drop_index:
+            logger.warning(f"Dropping {elasticsearch_index}.")
+            es_client.indices.delete(elasticsearch_index)
+            logger.info(f"Creating {elasticsearch_index}.")
+            es_client.indices.create(
+                index=elasticsearch_index, body=TWEET_MAPPING
+            )
+    else:
+        logger.info(f"Creating {elasticsearch_index}.")
+        es_client.indices.create(index=elasticsearch_index, body=TWEET_MAPPING)
+        logger.info(f"{elasticsearch_index} successfully created.")
+
+    api = twitter.Api(
+        consumer_key=twitter_consumer_key,
+        consumer_secret=twitter_consumer_secret,
+        access_token_key=twitter_access_token_key,
+        access_token_secret=twitter_access_token_secret,
+    )
+
+    logger.info(f"Connecting to twitter stream. Tracking {', '.join(track)}.")
+    tweet_stream = api.GetStreamFilter(track=track)
+
+    tweet_to_bulk = curry(_tweet_to_bulk)(elasticsearch_index)
+    tweet_doc_stream = thread_last(
+        tweet_stream,
+        # Filter out tweets that don't contain profanity.
+        (filter, _contains_profanity),
+        # Convert the tweets to a bulk-indexable document.
+        (map, tweet_to_bulk),
+        # Partition for bulk writes.
+        (partition_all, batch_size),
+    )
+
+    logger.info(f"Sending tweets to {elasticsearch_index}.")
+    failed = 0
+    succeeded = 0
+    # Since the doc stream is partitioned we get the tweets in batches.
+    for tweet_batch in tweet_doc_stream:
+        if (failed + succeeded) % 100 == 0:
+            logger.info(
+                f"{failed + succeeded} tweets processed: "
+                f"{succeeded} succeeded, {failed} failed."
+            )
+        ok, fail = es_bulk(es_client, tweet_batch, stats_only=True)
+        succeeded += ok
+        failed += fail
+    pass  # TODO: Implement.
--- profanity_power_index.py
+++ profanity_power_index.py
@@ -1,13 +1,12 @@
 import click
 import os
-import twitter
 import elasticsearch
 import sys
 
 from dotenv import load_dotenv, find_dotenv
-from elasticsearch.helpers import bulk as es_bulk
 from loguru import logger
-from toolz import curry, get_in, thread_last, partition_all
+
+from collect_tweets import collect_tweets
 
 load_dotenv(find_dotenv())
 
@@ -26,68 +25,6 @@ if (
     or (not TWITTER_ACCESS_TOKEN_SECRET)
 ):
     logger.warn("Missing Twitter API keys - collection will not function.")
-
-
-TWEET_MAPPING = {
-    "mappings": {
-        "tweet": {
-            "properties": {
-                "coordinates": {"type": "geo_point"},
-                "created_at": {
-                    "type": "date",
-                    "format": "EEE MMM dd HH:mm:ss Z yyyy",
-                },
-            }
-        }
-    }
-}
-
-
-PROFANITY = [
-    "fuck",
-    "shit",
-    "bitch",
-    "dick",
-    "douche",
-    " ass ",
-    "asshole",
-    "asshat",
-    "jackass",
-    "dumbass",
-]
-
-
-def _extract_text(tweet):
-    if "retweeted_status" in tweet:
-        if get_in(["retweeted_status", "truncated"], tweet, False):
-            return tweet["retweeted_status"]["extended_tweet"]["full_text"]
-        else:
-            return tweet["retweeted_status"]["text"]
-    else:
-        return tweet["text"]
-
-
-def _contains_profanity(tweet):
-    tweet_text = _extract_text(tweet).lower()
-    for profanity in PROFANITY:
-        if profanity in tweet_text:
-            return True
-
-    return False
-
-
-def _tweet_to_bulk(index, tweet):
-    return {
-        "_index": index,
-        "_type": "tweet",
-        "_id": tweet["id_str"],
-        "_source": {
-            "id": tweet["id_str"],
-            "coordinates": get_in(["coordinates", "coordinates"], tweet, None),
-            "text": _extract_text(tweet),
-            "created_at": tweet["created_at"],
-        },
-    }
 
 
 @click.group()
@@ -110,52 +47,17 @@ def collect(track, elasticsearch_index, drop_index, batch_size):
 
     es = elasticsearch.Elasticsearch(hosts=[ELASTICSEARCH_HOST])
 
-    if es.indices.exists(elasticsearch_index):
-        logger.warning(f"Index {elasticsearch_index} exists.")
-        if drop_index:
-            logger.warning(f"Dropping {elasticsearch_index}.")
-            es.indices.delete(elasticsearch_index)
-            logger.info(f"Creating {elasticsearch_index}.")
-            es.indices.create(index=elasticsearch_index, body=TWEET_MAPPING)
-    else:
-        logger.info(f"Creating {elasticsearch_index}.")
-        es.indices.create(index=elasticsearch_index, body=TWEET_MAPPING)
-        logger.info(f"{elasticsearch_index} successfully created.")
-
-    api = twitter.Api(
-        consumer_key=TWITTER_CONSUMER_KEY,
-        consumer_secret=TWITTER_CONSUMER_SECRET,
-        access_token_key=TWITTER_ACCESS_TOKEN_KEY,
-        access_token_secret=TWITTER_ACCESS_TOKEN_SECRET,
+    collect_tweets(
+        es,
+        track,
+        twitter_consumer_key=TWITTER_CONSUMER_KEY,
+        twitter_consumer_secret=TWITTER_CONSUMER_SECRET,
+        twitter_access_token_key=TWITTER_ACCESS_TOKEN_KEY,
+        twitter_access_token_secret=TWITTER_ACCESS_TOKEN_SECRET,
+        elasticsearch_index=elasticsearch_index,
+        drop_index=drop_index,
+        batch_size=batch_size
     )
-
-    logger.info(f"Connecting to twitter stream. Tracking {', '.join(track)}.")
-    tweet_stream = api.GetStreamFilter(track=track)
-
-    tweet_to_bulk = curry(_tweet_to_bulk)(elasticsearch_index)
-    tweet_doc_stream = thread_last(
-        tweet_stream,
-        # Filter out tweets that don't contain profanity.
-        (filter, _contains_profanity),
-        # Convert the tweets to a bulk-indexable document.
-        (map, tweet_to_bulk),
-        # Partition for bulk writes.
-        (partition_all, batch_size)
-    )
-
-    logger.info(f"Sending tweets to {elasticsearch_index}.")
-    failed = 0
-    succeeded = 0
-    # Since the doc stream is partitioned we get the tweets in batches.
-    for tweet_batch in tweet_doc_stream:
-        if (failed + succeeded) % 100 == 0:
-            logger.info(
-                f"{failed + succeeded} tweets processed: "
-                f"{succeeded} succeeded, {failed} failed."
-            )
-        ok, fail = es_bulk(es, tweet_batch, stats_only=True)
-        succeeded += ok
-        failed += fail
 
 
 if __name__ == "__main__":
