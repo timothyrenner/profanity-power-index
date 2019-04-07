b63868cfd2a2764029c2624f892147a008985e2a
Tim Renner <renner.timothy@gmail.com>
2019-04-07 16:03:29 -0500

    Implemented the extraction code.

---
+++ extract_profanity.py
@@ -0,0 +1,134 @@
+from toolz import assoc, assoc_in, thread_first, get_in
+from itertools import product
+from loguru import logger
+
+
+PROFANITY_MAPPING = {
+    "fuck": "text:*fuck*",
+    "shit": "text:*shit*",
+    "bitch": "text:bitch*",
+    "ass": "text:ass*",
+    "dick": "text:*dick*",
+    "douche": "text:douche*",
+}
+
+
+tweets_per_minute = {
+    "tweets_per_minute": {
+        "date_histogram": {
+            "field": "created_at",
+            "interval": "minute",
+            "format": "date_time_no_millis",
+        }
+    }
+}
+
+
+def profanity_filter(profanity_mapping):
+    return {
+        "profanity": {
+            "filters": {
+                "filters": {
+                    word: {"query_string": {"query": query}}
+                    for word, query in profanity_mapping.items()
+                }
+            }
+        }
+    }
+
+
+def time_range(start, end):
+    return {
+        "range": {
+            "created_at": {
+                "gte": start,
+                "lte": end,
+                "format": "date_time_no_millis",
+            }
+        }
+    }
+
+
+def target_match(target_query):
+    return {"query_string": {"query": target_query, "fields": ["text"]}}
+
+
+def target_filter(targets):
+    return {
+        "target": {
+            "filters": {
+                "filters": {
+                    target: target_match(f"{target}*") for target in targets
+                }
+            }
+        }
+    }
+
+
+def elasticsearch_query(start, end, targets, profanity_mapping):
+    return thread_first(
+        {"size": 0},
+        (assoc, "query", time_range(start, end)),
+        (assoc, "aggregations", tweets_per_minute),
+        (
+            assoc_in,
+            ["aggregations", "tweets_per_minute", "aggregations"],
+            profanity_filter(profanity_mapping),
+        ),
+        (
+            assoc_in,
+            [
+                "aggregations",
+                "tweets_per_minute",
+                "aggregations",
+                "profanity",
+                "aggregations",
+            ],
+            target_filter(targets),
+        ),
+    )
+
+
+def extract_profanity(
+    es_connection,
+    start,
+    end,
+    targets,
+    elasticsearch_index="profanity-power-index",
+):
+
+    es_query = elasticsearch_query(start, end, targets, PROFANITY_MAPPING)
+
+    logger.info("Calling elasticsearch.")
+    results = es_connection.search(index=elasticsearch_index, body=es_query)
+    logger.info(f"Done. Hit count: {results['hits']['total']}")
+
+    logger.info("Marshalling elasticsearch results.")
+    marshalled_results = []
+    time_profanity_target = product(
+        results["aggregations"]["tweets_per_minute"]["buckets"],
+        PROFANITY_MAPPING.keys(),
+        targets,
+    )
+    for time_bucket, profanity, target in time_profanity_target:
+        marshalled_results.append(
+            {
+                "time": time_bucket["key_as_string"],
+                "word": profanity,
+                "subject": target,
+                "count": get_in(
+                    [
+                        "profanity",
+                        "buckets",
+                        profanity,
+                        "target",
+                        "buckets",
+                        target,
+                        "doc_count",
+                    ],
+                    time_bucket,
+                    0,
+                ),
+            }
+        )
+    return marshalled_results
--- profanity_power_index.py
+++ profanity_power_index.py
@@ -2,11 +2,16 @@ import click
 import os
 import elasticsearch
 import sys
+import csv
+import re
 
 from dotenv import load_dotenv, find_dotenv
 from loguru import logger
+from datetime import datetime
+from dateutil import tz
 
 from collect_tweets import collect_tweets
+from extract_profanity import extract_profanity
 
 load_dotenv(find_dotenv())
 
@@ -24,7 +29,7 @@ if (
     or (not TWITTER_ACCESS_TOKEN_KEY)
     or (not TWITTER_ACCESS_TOKEN_SECRET)
 ):
-    logger.warn("Missing Twitter API keys - collection will not function.")
+    logger.warn("Missing Twitter API keys - collect will not function.")
 
 
 @click.group()
@@ -42,7 +47,7 @@ def main():
 def collect(track, elasticsearch_index, drop_index, batch_size):
 
     if not track:
-        logger.error("Must track at least one term.")
+        logger.error("❌ Must track at least one term. ❌")
         sys.exit(1)
 
     es = elasticsearch.Elasticsearch(hosts=[ELASTICSEARCH_HOST])
@@ -56,8 +61,54 @@ def collect(track, elasticsearch_index, drop_index, batch_size):
         twitter_access_token_secret=TWITTER_ACCESS_TOKEN_SECRET,
         elasticsearch_index=elasticsearch_index,
         drop_index=drop_index,
-        batch_size=batch_size
+        batch_size=batch_size,
     )
+
+
+@main.command()
+@click.argument("start", type=str)
+@click.argument("end", type=str)
+@click.option("--track", "-t", multiple=True)
+@click.option(
+    "--elasticsearch-index", "-e", type=str, default="profanity-power-index"
+)
+@click.option("--output", "-o", type=click.File("w"), default="-")
+def extract(start, end, track, elasticsearch_index, output):
+    if not track:
+        logger.error("❌ Must track at least one term. ❌")
+        sys.exit(1)
+
+    if not re.match(r".*\d{4}$", start):
+        time_zone = tz.gettz()
+        logger.info(f"Adding local time zone to start.")
+        start_date = datetime.strptime(start, "%Y-%m-%dT%H:%M:%S").replace(
+            tzinfo=time_zone
+        )
+        start = start_date.strftime("%Y-%m-%dT%H:%M:%S%z")
+
+    if not re.match(r".*\d{4}$", end):
+        time_zone = tz.gettz()
+        logger.info(f"Adding local time zone to end.")
+        end_date = datetime.strptime(end, "%Y-%m-%dT%H:%M:%S").replace(
+            tzinfo=time_zone
+        )
+        end = end_date.strftime("%Y-%m-%dT%H:%M:%S%z")
+
+    es = elasticsearch.Elasticsearch(hosts=[ELASTICSEARCH_HOST])
+    logger.info(
+        f"🖕 Extracting profanity between {start} and {end} "
+        f"for {', '.join(track)} in {elasticsearch_index}. 🖕"
+    )
+    results = extract_profanity(es, start, end, track)
+    logger.info(f"Writing to {output.name}.")
+
+    writer = csv.DictWriter(
+        output, fieldnames=["time", "word", "subject", "count"]
+    )
+    writer.writeheader()
+    for result in results:
+        writer.writerow(result)
+    logger.info(f"🖕 Wrote {len(results)} rows to {output.name}. 🖕")
 
 
 if __name__ == "__main__":
