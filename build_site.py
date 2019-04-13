f43280e3155bf6491d86efd4491df9f7aae8a8c1
Tim Renner <renner.timothy@gmail.com>
2019-04-13 18:21:35 -0500

    Make the color schemes a little more robust.

--- build_site.py
+++ build_site.py
@@ -1,6 +1,8 @@
 from toolz import assoc
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from palettable.colorbrewer import sequential as brewer
+from palettable.colorbrewer import sequential as brewer_sequential
+from palettable.colorbrewer import diverging as brewer_diverging
+from palettable.colorbrewer import qualitative as brewer_qualitative
 
 env = Environment(
     loader=FileSystemLoader("."), autoescape=select_autoescape(["html"])
@@ -8,7 +10,16 @@ env = Environment(
 
 
 def _make_colors(subject_config):
-    color_scheme = getattr(brewer, f"{subject_config['colors']}_5")
+    scheme_name = f"{subject_config['colors']}_5"
+    if hasattr(brewer_sequential, scheme_name):
+        color_scheme = getattr(brewer_sequential, scheme_name)
+    elif hasattr(brewer_diverging, scheme_name):
+        color_scheme = getattr(brewer_diverging, scheme_name)
+    elif hasattr(brewer_qualitative, scheme_name):
+        color_scheme = getattr(brewer_qualitative, scheme_name)
+    else:
+        raise ValueError(f"{scheme_name} is not a supported color scheme.")
+
     return {
         "sparkline": [
             {"offset": offset, "color": color}
@@ -18,8 +29,8 @@ def _make_colors(subject_config):
         ],
         "barchart": {
             "base": color_scheme.hex_colors[3],
-            "hover": color_scheme.hex_colors[4]
-        }
+            "hover": color_scheme.hex_colors[4],
+        },
     }
 
 
